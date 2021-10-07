f9ceace7d4c925381475c29d46f0ad0eaf3e4151
chemicstry <chemicstry@gmail.com>
2021-10-07 17:42:56 +0100

    Add lock benchmarks

---
+++ examples/lock_benchmark_fast.rs
@@ -0,0 +1,46 @@
+// This example benchmarks how long it takes to lock/unlock shared resource without contention.
+// On Raspberry Pi 4 this takes 310ns/170ns, most of it could be the Instant::now() vDSO overhead.
+
+#[rtic::app]
+mod app {
+    use std::time::{Instant, Duration};
+
+    const SAMPLES: u32 = 1_000_000;
+
+    #[shared]
+    struct Shared {
+        a: u32,
+    }
+
+    #[local]
+    #[derive(Default)]
+    struct Local {
+        taking: Duration,
+        releasing: Duration,
+    }
+
+    #[init]
+    fn init(_: init::Context) -> (Shared, Local, init::Monotonics) {
+        task1::spawn(0).unwrap();
+
+        (Shared { a: 0 }, Local { taking: Default::default(), releasing: Default::default() }, init::Monotonics())
+    }
+
+    #[task(shared = [a], local = [taking, releasing])]
+    fn task1(mut cx: task1::Context, x: u32) {
+        let mut start = Instant::now();
+        cx.shared.a.lock(|a| {
+            *cx.local.taking += start.elapsed();
+            *a += 1;
+            start = Instant::now();
+        });
+        *cx.local.releasing += start.elapsed();
+
+        if x < SAMPLES {
+            task1::spawn(x + 1).unwrap();
+        } else {
+            println!("Taking: {:?}", *cx.local.taking / SAMPLES);
+            println!("Releasing: {:?}", *cx.local.releasing / SAMPLES);
+        }
+    }
+}
---
+++ examples/lock_benchmark_slow.rs
@@ -0,0 +1,53 @@
+// This example benchmarks how long it takes to unlock shared resource when someone is waiting on it.
+// On Raspberry Pi 4 this takes 5.5us.
+
+#[rtic::app]
+mod app {
+    use std::time::{Instant, Duration};
+
+    const SAMPLES: u32 = 20_000;
+
+    #[shared]
+    struct Shared {
+        a: u32,
+    }
+
+    #[local]
+    #[derive(Default)]
+    struct Local {
+        releasing: Duration,
+    }
+
+    #[init]
+    fn init(_: init::Context) -> (Shared, Local, init::Monotonics) {
+        task1::spawn(0).unwrap();
+
+        (Shared { a: 0 }, Local { releasing: Default::default() }, init::Monotonics())
+    }
+
+    #[task(priority = 2, shared = [a], local = [releasing])]
+    fn task1(mut cx: task1::Context, x: u32) {
+        let start = cx.shared.a.lock(|a| {
+            *a += 1;
+
+            if x < SAMPLES {
+                task2::spawn(x + 1).unwrap();
+            } else {
+                println!("Releasing: {:?}", *cx.local.releasing / SAMPLES);
+            }
+
+            // Wait for task2 to actually block on the resource
+            std::thread::sleep(Duration::from_micros(20));
+            Instant::now()
+        });
+        *cx.local.releasing += start.elapsed();
+    }
+
+    #[task(shared = [a])]
+    fn task2(mut cx: task2::Context, x: u32) {
+        cx.shared.a.lock(|a| {
+            *a += 1;
+        });
+        task1::spawn(x).unwrap();
+    }
+}
